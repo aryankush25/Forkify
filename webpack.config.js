aec0b33cec93618040f1d21233bcb67ce14ef8cb
Aryan Agarwal <aryankush025@gmail.com>
2019-11-10 17:52:33 +0530

    MERGE CONFLICTS RESOLVED

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,10 @@ const path = require('path')
 module.exports = {
     entry: './src/js/index.js',
     output: {
-<<<<<<< HEAD
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
     },
     devServer: {
         contentBase: './dist'
-=======
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
->>>>>>> a34301828e77147e9499903013e8235761341be7
     }
 };
